96bce90be59ec24548420b352e40e906ab96c795
ftiannisa <firstiannisa.02@gmail.com>
2023-06-15 02:20:58 +0700

    fungsi login

--- templates/header.php
+++ templates/header.php
@@ -1,29 +1,44 @@
+<?php
+$homeUrl = 'index.php';
+$btnUrl = 'login.php';
+$btnText = 'LOGIN';
+$btnClass = "btn btn-outline-primary";
+
+if (isset($_SESSION['name'])) {
+  $homeUrl = 'dashboard.php';
+  $btnUrl = 'logout.php';
+  $btnText = 'LOGOUT';
+  $btnClass = "btn btn-danger";
+}
+?>
+
+<!-- layout -->
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <link rel="stylesheet" href="templates\css\style.css" />
+    <link rel="stylesheet" href="templates/css/style.css" />
 
     <!-- google font -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 
-    <!-- font Ubuntu -->
-    <link
-      href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@500&display=swap"
-      rel="stylesheet"
-    />
-
     <title>Document</title>
   </head>
   <body>
     <header>
-      <nav class="navbar navbar-light" style="background-color: #fcf267">
+      <nav
+        class="navbar navbar-light autohide"
+        style="background-color: #fffee6"
+      >
         <div class="container-fluid">
           <!-- school logo -->
-          <a class="navbar-brand d-flex align-items-center" href="#">
+          <a
+            class="navbar-brand d-flex align-items-center"
+            href="<?php echo $homeUrl; ?>"
+          >
             <img
               src="https://img.icons8.com/stickers/100/module.png"
               alt=""
@@ -31,14 +46,17 @@
               height="50"
               class="d-inline-block align-text-top"
             />
-            <span class="ms-2" style="font-family: 'Ubuntu'"
-              >Sekolah Tinggi JeWePe</span
-            >
+            <span class="ms-2 logo-text">Sekolah Tinggi JeWePe</span>
           </a>
 
           <ul class="nav justify-content-end">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">HOME</a>
+              <a
+                class="nav-link active"
+                aria-current="page"
+                href="<?php echo $homeUrl; ?>"
+                >HOME</a
+              >
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">PROFIL</a>
@@ -50,9 +68,11 @@
               <a href="" class="nav-link"></a>
             </li>
             <li class="nav-item">
-              <button type="button" class="btn btn-outline-primary">
-                LOGIN
-              </button>
+              <a href="<?php echo $btnUrl; ?>">
+                <button type="button" class="<?php echo $btnClass; ?>">
+                  <?php echo $btnText; ?>
+                </button>
+              </a>
             </li>
           </ul>
         </div>
